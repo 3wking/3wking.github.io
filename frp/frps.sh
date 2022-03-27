9034d7252ab869c14b1b174e57b49c989a54e261
3wking <43069914+3wking@users.noreply.github.com>
2022-03-27 21:11:46 +0800

    Delete frps.sh

--- frp/frps.sh
+++
@@ -1,5 +0,0 @@
-!#/usr/bin/sh
-wget https://3wking.github.io/frp/frps.tar.gz &&
-tar zxvf frps.tar.gz -C /usr/local &&
-mv -f /usr/local/frps/systemd/frps.service  /usr/lib/systemd/system &&
-echo "OK"
