865862f09477f8b84a7b249badce287db8a30f95
3wking <43069914+3wking@users.noreply.github.com>
2022-03-27 21:33:50 +0800

    Update frps.sh

--- frp/frps.sh
+++ frp/frps.sh
@@ -1,5 +1,5 @@
 !#/usr/bin/sh
-wget https://3wking.github.io/frp/frps.tar.gz -O frps.tar.gz&&
-tar zxvf frps.tar.gz -C /usr/local&&
-mv -f /usr/local/frps/systemd/frps.service /usr/lib/systemd/system&&
-echo OK
+wget https://3wking.github.io/frp/frps.tar.gz -O frps.tar.gz
+&&tar zxvf frps.tar.gz -C /usr/local
+&&mv -f /usr/local/frps/systemd/frps.service /usr/lib/systemd/system
+&&echo OK
