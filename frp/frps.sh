a18c404fac5538bc7cbf1a736241f9431ddd7222
3wking <43069914+3wking@users.noreply.github.com>
2022-03-27 22:13:48 +0800

    Update frps.sh

--- frp/frps.sh
+++ frp/frps.sh
@@ -4,7 +4,6 @@ tar zxvf frps.tar.gz -C /usr/local &&
 mv -f /usr/local/frps/frps.service /usr/lib/systemd/system &&
 firewall-cmd --zone=public --add-port=7000/tcp --permanent &&
 firewall-cmd --zone=public --add-port=7001/tcp --permanent &&
-firewall-cmd --zone=public --add-port=7002/tcp --permanent &&
 firewall-cmd --zone=public --add-port=80/tcp --permanent &&
 firewall-cmd --zone=public --add-port=443/tcp --permanent &&
 echo OK
